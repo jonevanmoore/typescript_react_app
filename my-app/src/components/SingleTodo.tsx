8049c58a808f1edf830bdcfeef4d33b9c36fcbe6
Jon Moore <jonevanmoore@yahoo.com>
2022-10-18 10:47:46 -0700

    strike and delete

--- my-app/src/components/SingleTodo.tsx
+++ my-app/src/components/SingleTodo.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
+import TodoList from './TodoList'
 
 type Props = {
     todo: Todo,
@@ -11,17 +12,28 @@ type Props = {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo) =>
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+        ))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id))
+    }
+
     return (
         <form action="" className="todos_single">
-            <span className="todos_single-text">{todo.todo}</span>
+            {todo.isDone ? (<s className="todos_single--text">{todo.todo}</s>) : (<span className="todos_single--text">{todo.todo}</span>)}
             <div>
                 <span className="icon">
                     <AiFillEdit />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDelete(todo.id)}>
                     <AiFillDelete />
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={() => handleDone(todo.id)}>
                     <MdDone />
                 </span>
             </div>
